6b92df7249481edf9fac7f5445dc2ac3912aaadb
maahi02 <mahesh3thakur@gmail.com>
2025-02-02 01:56:36 +0545

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const API_BASE_URL = "https://tst.tryasp.net"; // Replace with your API URL
 async function login() {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
-
+const grant_type = "password";
     const response = await fetch(`${API_BASE_URL}/token`, {
         method: "POST",
         credentials: "include", // Ensures cookies are sent/received
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify({ username, password }),
+        body: JSON.stringify({ username, password, grant_type :"password" }),
     });
 
     const data = await response.json();
